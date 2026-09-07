453b4f00fcc99a5fa1390eb6192a9a070ea25d1d
AWS ParallelCluster user <ec2-user@ip-10-0-9-209.ap-southeast-1.compute.internal>
2026-09-07 04:36:01 +0000

    add new scripts

---
+++ rse-slurm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH -J "rse"
+#SBATCH -o rse-slurm.%N.%j.out
+#SBATCH -e rse-slurm.%N.%j.err
+
+python -u -c "import PyHipp as pyh; \
+import time; \
+import os; \
+t0 = time.time(); \
+print(time.localtime()); \
+os.chdir('sessioneye'); \
+pyh.RPLSplit(SkipLFP=False, SkipHighPass=False); \
+print(time.localtime()); \
+print(time.time()-t0);"
