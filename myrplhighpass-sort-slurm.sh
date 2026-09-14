73b69a656969bc4aed770bdd1c333d304179cf8f
AWS ParallelCluster user <ec2-user@ip-10-0-2-235.ap-southeast-1.compute.internal>
2026-09-14 03:57:38 +0000

    Backup Lab 5 scripts

---
+++ myrplhighpass-sort-slurm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH -J "rplhighpass-sort"
+#SBATCH -o rplhighpass-sort-slurm.%N.%j.out
+#SBATCH -e rplhighpass-sort-slurm.%N.%j.err
+
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=`/data/src/PyHipp/envlist.py`
+conda activate $envarg
+
+
+python -u -c "import PyHipp as pyh; \
+import time; \
+pyh.RPLHighPass(saveLevel=1); \
+from PyHipp import mountain_batch; \
+mountain_batch.mountain_batch(); \
+from PyHipp import export_mountain_cells; \
+export_mountain_cells.export_mountain_cells(); \
+print(time.localtime());"
+
+conda deactivate 
+/data/src/PyHipp/envlist.py $envarg
